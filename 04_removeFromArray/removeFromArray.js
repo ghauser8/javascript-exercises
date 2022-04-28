1603c6250555bbbb3ad189a3f9385d718ebb59c2
greg <ghauser8@gmail.com>
2022-04-28 15:24:16 -0400

    Completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let args = [];
+    for (let i = 0; i < arguments.length; i++) {
+        args[i] = arguments[i];
+    }
+    for (i = 0; i < args.length; i++) {
+        for (let j=0; j < arr.length; j++) {
+            if (arr[j] === args[i]) {
+                arr.splice(j,1)
+            }
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
